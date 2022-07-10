6e0075dd57d68796f75584875c4b019451b57c0a
JoaoGabrielRLP <joojgaag@gmail.com>
2022-07-09 22:49:04 -0300

    Calcular e exibir números primos

---
+++ mini-projeto10.js
@@ -0,0 +1,19 @@
+// Criar função para mostrar os números primos com
+
+exibirNumerosPrimos(11);
+
+function exibirNumerosPrimos(limite){
+    for(i = 2; i <= limite; i++){         
+        if(calcularNumerosPrimos(i)) console.log(i);    
+        
+    }
+}
+
+function calcularNumerosPrimos(i){
+    for(let divisor = 2; divisor < i; divisor++){
+        if(i % divisor === 0){
+            return false;
+        }
+    }
+    return true;
+}
